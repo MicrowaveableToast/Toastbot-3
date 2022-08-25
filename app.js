f25c30bccbf1f29dc53d580c77c9bb163911748e
MicrowaveableToast <68101424+MicrowaveableToast@users.noreply.github.com>
2022-08-25 16:44:23 -0700

    Update app.js

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
     let guildQueue = client.player.getQueue(message.guild.id);
+    if (args === null) return:
     if (command === 'play' || 'p') {
         let queue = client.player.createQueue(message.guild.id);
         await queue.join(message.member.voice.channel);
